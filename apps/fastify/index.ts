3c5db58352895c778dd1f1eb327c9ac47da05e96
davidasix <david@davidasix.com>
2026-05-18 18:17:32 -0400

    Connect Astro & Fastify to tRPC

--- apps/fastify/index.ts
+++ apps/fastify/index.ts
@@ -1,9 +1,16 @@
 import Fastify from "fastify";
+import FastifyCors from "@fastify/cors";
+import FastifyWebSocket from "@fastify/websocket";
+import { fastifyTRPCPlugin } from "@trpc/server/adapters/fastify";
+import { appRouter } from "@midwifes-notebook/trpc";
 
 const fastify = Fastify({ logger: true });
 
-fastify.get("/", async () => {
-  return { message: "Hello World!" };
+fastify.register(FastifyCors);
+fastify.register(FastifyWebSocket);
+fastify.register(fastifyTRPCPlugin, {
+  prefix: "/trpc",
+  trpcOptions: { router: appRouter },
 });
 
 await fastify.listen({ port: 3001, host: "0.0.0.0" });
--- packages/trpc/index.ts
+++ packages/trpc/index.ts
@@ -1,1 +1,11 @@
-export {};
+import { initTRPC } from "@trpc/server";
+
+const t = initTRPC.create();
+
+export const appRouter = t.router({
+  hello: t.procedure.query(() => {
+    return "Hello World!";
+  }),
+});
+
+export type AppRouter = typeof appRouter;
