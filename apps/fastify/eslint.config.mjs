63113f6af77fcf7c1d7cdab23649190f03b53436
davidasix <david@davidasix.com>
2026-05-18 18:42:21 -0400

    Move server to fastify and web to astro

--- apps/fastify/eslint.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "eslint/config";
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
-
-export default defineConfig([
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-]);
