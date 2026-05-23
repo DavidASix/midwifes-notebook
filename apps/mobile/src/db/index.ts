551814323063ab1002513de0bae1f3e57044a7d9
davidasix <david@davidasix.com>
2026-05-23 19:03:08 -0400

    Add handler for db re-generation

--- apps/mobile/src/db/index.ts
+++ apps/mobile/src/db/index.ts
@@ -1,9 +1,15 @@
 import { drizzle } from "drizzle-orm/expo-sqlite";
-import { openDatabaseSync } from "expo-sqlite";
+import { deleteDatabaseSync, openDatabaseSync } from "expo-sqlite";
 import * as schema from "./schema";
 
-const expo = openDatabaseSync("midwifes_notebook.db", {
-  enableChangeListener: true,
-});
+const DB_NAME = "midwifes_notebook.db";
+
+let expo = openDatabaseSync(DB_NAME, { enableChangeListener: true });
+// Delete the database in development mode to ensure a clean slate on each reload. Stop using this after shipping 1 migration.
+if (__DEV__) {
+  expo.closeSync();
+  deleteDatabaseSync(DB_NAME);
+  expo = openDatabaseSync(DB_NAME, { enableChangeListener: true });
+}
 
 export const db = drizzle(expo, { schema });
