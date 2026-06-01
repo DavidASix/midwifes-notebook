877f6bc3c50605a59b7003d75b3787a91b87ca6b
davidasix <david@davidasix.com>
2026-06-01 11:11:55 -0400

    Install Lucide icon

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { Stack } from "expo-router";
+import { router, Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 // import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
 
 // import { db } from "../src/db";
 // import migrations from "../drizzle/migrations";
+import { Pressable } from "react-native";
+import { ArrowBigLeft } from "lucide-react-native";
 import { useAppFonts } from "@/hooks/useAppFonts";
 import { ThemeProvider, useTheme } from "@/lib/theme-context";
 import { screenOptions } from "@/lib/themes";
@@ -16,7 +18,16 @@ function RootLayoutContent() {
   const theme = useTheme();
   return (
     <>
-      <Stack screenOptions={screenOptions(theme)}>
+      <Stack
+        screenOptions={{
+          ...screenOptions(theme),
+          headerLeft: () => (
+            <Pressable onPress={() => router.back()} hitSlop={8}>
+              <ArrowBigLeft color={theme.primary} size={28} />
+            </Pressable>
+          ),
+        }}
+      >
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
       <StatusBar style={theme.statusBarIcons} />
